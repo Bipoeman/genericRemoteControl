282a18faa904a7694f60580aeb556661f6f8d3bf
Chanathip <bipoeman12@gmail.com>
2024-02-09 01:55:51 +0700

    merge media and slide control

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const Player = require("winplayer-node");
 var mqtt = require('mqtt');
 const audio = require('win-audio');
+var robot = require('robotjs')
 
+require('dotenv').config()
 
-const MQTT_SERVER = "api.easyfarming.net";
-const MQTT_PORT = "1883";
+const MQTT_SERVER = process.env.MQTT_SERVER;
+const MQTT_PORT = process.env.MQTT_PORT;
 //if your server don't have username and password let blank.
 const MQTT_USER = "";
 const MQTT_PASSWORD = "";
@@ -32,6 +34,11 @@ client.on('connect', function () {
             console.log(err);
         }
     });
+    client.subscribe('/presentControl/presentControl/#', function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
 });
 
 
@@ -40,41 +47,16 @@ let player;
 // player = Player.Play()
 let lastState
 function onUpdate() {
-    const update = player.getUpdate();
-    update.then((mediaStatus) => {
-        // mediaStatus
-        try {
-            // console.log(mediaStatus['status']) 
-            // console.log(mediaStatus['metadata']['title'])
-            // console.log(`{
-            //     'status':${mediaStatus['status']},
-            //     'title':${mediaStatus['metadata']['title']}
-            // }`)
-            // console.log(mediaStatus['status'])
-            // if (lastState != mediaStatus['status']){
-            // console.log("Media Status Updated")
-            // console.log(mediaStatus['status'])
+    try {
+        const update = player.getUpdate();
+        update.then((mediaStatus) => {
             lastState = mediaStatus['status'];
             client.publish('/mediaControl/mediaStatus/status', lastState, { retain: true, qos: 2 })
-            // client.publish('/mediaControl/mediaStatus/status', mediaStatus['status'], { retain: true, qos: 2 })
-            // client.publish('/mediaControl/mediaStatus/status', mediaStatus['status'], { retain: true, qos: 2 })
-            // "title":"${mediaStatus['metadata']['title']}"
-            // }
-            // else{
-            //     console.log("Just repeated media Status")
-            // }
-            // client.publish('/mediaControl/mediaControl/volume',mediaStatus['status'],{qos:2}) 
-
-            // client.publish('/mediaControl/mediaStatus/status',mediaStatus['status'])
-            // client.publish('/mediaControl/mediaStatus/title',mediaStatus['metadata']['title'])
-
-        }
-        catch {
-            console.log("Media get update Error")
-        }
-
-    });
-    // player.Play()
+        });
+    }
+    catch {
+        console.log("Media get update Error")
+    }
 }
 
 player = new Player(onUpdate);
@@ -85,46 +67,62 @@ client.on('message', function (topic, message) {
     // message is Buffer
     // console.log(topic)
     // console.log(message.toString());
-    if (message.toString() == "Toggle") {
-        console.log(lastState)
-        player.PlayPause()
-        // if (lastState == "Playing"){
-        //         // setTimeout(()=>{})
-        //         player.Pause()
-        // }
-        // if (lastState == "Paused"){
-        //         player.Play()
-        // }
-    }
-
-    if (message.toString() == "Next") {
-        player.Next()
-    }
-    if (message.toString() == "Previous") {
-        player.Previous()
-    }
-    if (topic == "/mediaControl/mediaControl/volume") {
-        currentVol = speaker.get()
-        // console.log(currentVol)
-        if (message.toString() == "Up") {
-            speaker.set(currentVol + 2)
-        }
-        if (message.toString() == "Down") {
-            speaker.set(currentVol - 2)
-        }
-        if (message.toString() == "Off") {
-
-            if (speaker.isMuted()) {
-                speaker.unmute()
+    var baseTopic = topic.split("/")[1]
+    try {
+        if (baseTopic == "mediaControl") {
+            if (message.toString() == "Toggle") {
+                console.log(lastState)
+                player.PlayPause()
             }
-            else {
-                speaker.mute()
+
+            if (message.toString() == "Next") {
+                player.Next()
             }
-            // client.publish('/mediaControl/mediaStatus/volume',``,{qos:2}) 
+            if (message.toString() == "Previous") {
+                player.Previous()
+            }
+            if (topic == "/mediaControl/mediaControl/volume") {
+                currentVol = speaker.get()
+                // console.log(currentVol)
+                if (message.toString() == "Up") {
+                    speaker.set(currentVol + 2)
+                }
+                if (message.toString() == "Down") {
+                    speaker.set(currentVol - 2)
+                }
+                if (message.toString() == "Off") {
+                    if (speaker.isMuted()) {
+                        speaker.unmute()
+                    }
+                    else {
+                        speaker.mute()
+                    }
+                    // client.publish('/mediaControl/mediaStatus/volume',``,{qos:2}) 
+                }
+                client.publish('/mediaControl/mediaStatus/volume', `${speaker.get().toString()},${speaker.isMuted().toString()}`, { qos: 2, retain: true })
+            }
         }
-        client.publish('/mediaControl/mediaStatus/volume', `${speaker.get().toString()},${speaker.isMuted().toString()}`, { qos: 2, retain: true })
-        // client.publish('/mediaControl/mediaStatus/volume', `${speaker.get().toString()},${speaker.isMuted().toString()}`, { qos: 2, retain: true })
-        // client.publish('/mediaControl/mediaStatus/volume', `${speaker.get().toString()},${speaker.isMuted().toString()}`, { qos: 2, retain: true })
+        else if (baseTopic == "presentControl"){
+            if (message.toString() === "Next"){
+                console.log('right')
+                robot.keyTap('right')
+            }
+            if (message.toString() === "Previous"){
+                console.log('left')
+                robot.keyTap('left')
+            }
+            if (message.toString() === "Exit"){
+                console.log('escape')
+                robot.keyTap('escape')
+            }
+            if (message.toString() === "Show"){
+                console.log('f5')
+                robot.keyTap('f5')
+            }
+        }
+    }
+    catch {
+
     }
     // if (message.toString() == "Play"){
     //     player.Play()
